d1584070b1756030e585b27b47e25296dbcca9df
Kristina Romanova <hrusha@freenet.de>
2023-09-16 13:45:10 +0200

    build

--- src/main/java/org/ait/demo/fw/ApplicationManager.java
+++ src/main/java/org/ait/demo/fw/ApplicationManager.java
@@ -3,6 +3,7 @@ package org.ait.demo.fw;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
